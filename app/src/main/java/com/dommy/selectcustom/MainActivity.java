64f63dac29397e91f36f3feb75e8d64863880ff9
yangdong <ahuyangdong@163.com>
2020-07-29 17:29:17 +0800

    添加自适应布局功能，更新演示动画

--- app/src/main/java/com/dommy/selectcustom/MainActivity.java
+++ app/src/main/java/com/dommy/selectcustom/MainActivity.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.CheckBox;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.dommy.selectcustom.util.CommonUtil;
 
@@ -22,6 +21,8 @@ public class MainActivity extends AppCompatActivity {
     List<CheckBox> radios; // 单选组
     @BindViews({R.id.checkbox1, R.id.checkbox2, R.id.checkbox3, R.id.checkbox4, R.id.checkbox5, R.id.checkbox6, R.id.checkbox7})
     List<CheckBox> checkBoxes; // 多选组
+    @BindViews({R.id.flex1, R.id.flex2, R.id.flex3, R.id.flex4, R.id.flex5, R.id.flex6})
+    List<CheckBox> flexs; // 自适应单选组
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,6 +58,20 @@ public class MainActivity extends AppCompatActivity {
     void changeCheckBoxs(CheckBox checkBox) {
         // 显示选中项值
         String checkedValues = CommonUtil.getMany(checkBoxes);
+        tvValue.setText("选中了：" + checkedValues);
+    }
+
+    /**
+     * 自适应单选项点击事件
+     * @param checkBox
+     */
+    @OnClick({R.id.flex1, R.id.flex2, R.id.flex3, R.id.flex4, R.id.flex5, R.id.flex6})
+    void changeFlexs(CheckBox checkBox) {
+        CommonUtil.unCheck(flexs);
+        checkBox.setChecked(true);
+
+        // 显示选中项值
+        String checkedValues = CommonUtil.getOne(flexs);
         tvValue.setText("选中了：" + checkedValues);
     }
 }
